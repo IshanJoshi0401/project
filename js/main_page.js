b05283259086f1437f133a7166191a5007b5af33
IshanJoshi0401 <ishan.joshi4693@gmail.com>
2024-03-22 09:56:44 -0400

    3rd commit

--- js/main_page.js
+++ js/main_page.js
@@ -12,16 +12,19 @@ function myAccFunc() {
   }
 }
 
-function myDropFunc() {
-  var x = document.getElementById("demoDrop");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-    x.previousElementSibling.className += " w3-green";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-    x.previousElementSibling.className = x.previousElementSibling.className.replace(
-      " w3-green",
-      ""
-    );
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
   }
 }
